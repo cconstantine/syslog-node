6796f6dd036dace034790f448ca3d7666f6e01a0
Chris Constantine <cconstan@gmail.com>
2011-07-30 15:52:07 -0700

    now with more login

--- server.js
+++ server.js
@@ -1,38 +1,76 @@
-var sockets;
+var net     = require('net');
+var express = require('express');
+var app     = express.createServer();
+var io      = require('socket.io');
+var MemoryStore = express.session.MemoryStore;
+var sessionStore = new MemoryStore();
 
-var net = require('net');
+app.configure(function () {
+	app.use(express.static(__dirname + '/public'));
+	app.use(express.cookieParser());
+	app.use(express.bodyParser());
+	app.use(express.session({secret: 'secret', key: 'express.sid'}));
+	app.use(app.router);
+
+    });
+sio = io.listen(app);
+
+
+
+app.listen(80);
+sio.set('log level', 1); 
+
+
+app.get('/', function(req, res){
+	res.render('index.ejs');
+});
+
+app.get('/login', function(req, res) {
+	res.render('login.ejs');
+    });
+
+app.post('/login', function(req, res) {
+	console.log(req)
+	req.session.username = req.params.username;
+	res.redirect('/login');
+    });
+
+var parseCookie = require('connect').utils.parseCookie;
+ 
+sio.set('authorization', function (data, accept) {
+    // check if there's a cookie header
+    if (data.headers.cookie) {
+        // if there is, parse the cookie
+        data.cookie = parseCookie(data.headers.cookie);
+        // note that you will need to use the same key to grad the
+        // session id, as you specified in the Express setup.
+        data.sessionID = data.cookie['express.sid'];
+	/// (literally) get the session data from the session store
+        sessionStore.get(data.sessionID, function (err, session) {
+		if (err) {
+		    // if we cannot grab a session, turn down the connection
+		    accept(err.message, false);
+		} else {
+		    // save the session data and accept the connection
+		    data.session = session;
+		    console.log('session: ' + session);
+		    accept("not logged in", false);
+		}
+	    });
+	
+    } else {
+       // if there isn't, turn down the connection with a message
+       // and leave the function.
+       return accept('No cookie transmitted.', false);
+    }
+});
+
+/* Syslog server */
 var server = net.createServer(function (stream) {
-	console.log('hi');
 	stream.setEncoding('utf8');
 	stream.addListener("data", function (data) {
-		console.log(data);
-		if (sockets) {
-		    sockets.emit('logs', data);
-		}
+		sio.sockets.emit('logs', data);
 	    });
     });	
 server.listen(514, 'localhost');
-
-
-var express = require('express');
-
-var app = express.createServer();
-var io = require('socket.io').listen(app);
-
-app.use(express.static(__dirname + '/public'));
-app.use(app.router);
-
-app.get('/', function(req, res){
-    res.render('index.ejs', { title: 'My Site' });
-});
-
-app.listen(80);
-
-io.sockets.on('connection', function (socket) {
-  socket.emit('logs', 'hello world' );
-  sockets = socket;
-
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
+/* ************* */
