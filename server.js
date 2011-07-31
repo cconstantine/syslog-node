506f31ecd121cff11e1fc40e7b82a1179689951a
Chris Constantine <cconstan@gmail.com>
2011-07-31 00:03:03 -0700

    write log messages to mongo and return last 10 messages on socket connect

--- server.js
+++ server.js
@@ -2,8 +2,27 @@ var net     = require('net');
 var express = require('express');
 var app     = express.createServer();
 var io      = require('socket.io');
-var MemoryStore = express.session.MemoryStore;
-var sessionStore = new MemoryStore();
+var mongodb = require('mongodb');
+var MongoStore = require('connect-mongo');
+
+var mongo_config = {host: 'localhost', port: 27017, db: 'syslog-node'}
+
+var db = new mongodb.Db(mongo_config.db,
+		     new mongodb.Server("127.0.0.1", 27017),
+		     {native_parser:true});
+
+
+
+var sessionStore = new MongoStore({db : mongo_config.db});
+
+db.open(function(err, connection) {
+	if (err) {
+	    console.log('*****************');
+	    console.log(err.stack);
+	    console.log('*****************');
+	    process.exit(1);
+	}
+	
 
 app.configure(function () {
 	app.use(express.static(__dirname + '/public'));
@@ -13,30 +32,41 @@ app.configure(function () {
 			secret: 'secret', key: 'express.sid'}));
 	app.use(app.router);
 
-    });
+    }); 
 sio = io.listen(app);
-
-
-
-app.listen(80);
-sio.set('log level', 1); 
 
 
 app.get('/', function(req, res){
 	res.render('index.ejs');
-});
+    });
 
 app.get('/login', function(req, res) {
 	res.render('login.ejs');
     });
 
 app.post('/login', function(req, res) {
+	console.log(req.session);
 	req.session.username = req.body.username;
 	res.redirect(req.body.next);
     });
 
 var parseCookie = require('connect').utils.parseCookie;
  
+sio.sockets.on('connection', function(socket) {
+	console.log(1);
+	connection.collection('logs', function(err, col) {
+		console.log(2);
+		cur = col.find().sort(['created_at', 'desc']).limit(10);
+		cur.each(function(err, item) {
+			if (item) {
+			    console.log(3);
+			    console.log(item);
+			    socket.emit('logs', item.message);
+			}
+		    });
+	    });
+    });
+
 sio.set('authorization', function (data, accept) {
     // check if there's a cookie header
     if (data.headers.cookie) {
@@ -47,12 +77,13 @@ sio.set('authorization', function (data, accept) {
         data.sessionID = data.cookie['express.sid'];
 	/// (literally) get the session data from the session store
         sessionStore.get(data.sessionID, function (err, session) {
-		if (err) {
+		if (err || !session) {
 		    // if we cannot grab a session, turn down the connection
 		    accept(err.message, false);
 		} else if(session.username) {
 		    accept(null, true);
 		} else {
+		    console.log(session);
 		    accept("not logged in", false);
 		}
 	    });
@@ -64,12 +95,35 @@ sio.set('authorization', function (data, accept) {
     }
 });
 
+app.listen(80);
+sio.set('log level', 1); 
+
+
+
+
 /* Syslog server */
-var server = net.createServer(function (stream) {
-	stream.setEncoding('utf8');
-	stream.addListener("data", function (data) {
-		sio.sockets.emit('logs', data);
+	
+	connection.collection('logs', function(err, col) {
+		var i = [['created_at', -1]];
+		col.ensureIndex(i, function(err, indexName) {
+			console.log("created index: " + indexName);      
+		    })
 	    });
-    });	
-server.listen(514, 'localhost');
-/* ************* */
+
+	function write_message (message) {
+	    connection.collection('logs', function(err, col) {
+		    col.insert({message : message,
+                                created_at : new Date()});
+		});
+	}
+
+	var server = net.createServer(function (stream) {
+		stream.setEncoding('utf8');
+		stream.addListener("data", function (data) {
+			write_message(data);
+			sio.sockets.emit('logs', data);
+		    });
+	    });	
+	server.listen(514, 'localhost');
+	/* ************* */
+    });
